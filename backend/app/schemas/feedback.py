f3792365ca1f8817f72a1e8e0eb0103740dcb499
Renaud Amar <renaud.amar@gmail.com>
2025-07-17 11:13:31 +0200

    Don't validate user email in feedback endpoint
    We care more about gathering the actual feedback and we don't want email validation to get in the way of getting the feedback

--- backend/app/schemas/feedback.py
+++ backend/app/schemas/feedback.py
@@ -1,10 +1,12 @@
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel
 from typing import Optional
 
 
 class FeedbackRequest(BaseModel):
     message: str
-    email: Optional[EmailStr] = None
+    # we care more about gathering feedback than the email, so let's not
+    # validate it with EmailStr and just use a simple string:
+    email: Optional[str] = None
 
 
 class FeedbackResponse(BaseModel):
